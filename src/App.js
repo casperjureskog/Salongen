86e758f8c33f7dff6b48aaa0e8d5e543763f5e05
casperjureskog <lcasper@mac.com>
2018-06-28 11:22:22 +0200

    add icons and base functions for a menu

--- src/App.js
+++ src/App.js
@@ -4,22 +4,63 @@ import './App.css';
 import SalongList from './salongList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterMenuOn: false,
+      filterMenuState: 'emty'
+    };
+  }
+
+  header(){
+    return (
+      <header className="App-header">
+          <i class="material-icons gold">arrow_back_ios</i>
+          <h2>Hår</h2>
+          <i class="material-icons rotate gold">tune</i>
+      </header>
+    );
+  }
   
-  test(){
-    return 'nisse';
+  line() {
+    return (
+      <div class="gold"><hr></hr></div>
+    )
+  }
+  
+  filterMenu() {
+      if(this.state.filterMenuState === 'emty') {
+        return (
+          <button class="filterMenu" onClick={() => this.setState({ filterMenuOn: true, filterMenuState: 'menuDown' })}>
+            <h5>Pris filter</h5>
+            <i class="material-icons gold">expand_more</i>
+          </button>
+        )
+      } else {
+        this.filterMenuState
+      }
+  }
+  
+  filterList() {
+    return (
+      <ul>
+        <li>'Inget filter'</li>
+        <li>'Pris 0 - 250 kr'</li>
+        <li>'Pris 250 - 500 kr'</li>
+        <li>'Pris 500 - 1000 kr'</li>
+        <li>'Pris 1000 - Upp kr'</li>
+      </ul>
+    )
   }
     
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.test()} 
-          <MaterialIcon icon="dashboard" />
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.header()}
+        {this.line()}
+        {this.state.filterMenuOn && this.filterList()}
+        {this.state.filterMenuState === 'emty' && this.filterMenu()}
+        {this.line()}
         <ul>
          {
            SalongList.map(function(movie){
