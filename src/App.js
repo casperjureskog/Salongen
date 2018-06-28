31d36b1b2cf7a1c47dc0dbcdd4d72d89d5b22c98
casperjureskog <lcasper@mac.com>
2018-06-28 18:08:03 +0200

    add sorting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     super(props);
     this.state = {
       filterMenuOn: false,
-      filterMenuState: 'Pris filter'
+      filterMenuState: 'Pris filter',
+      interMin: 0,
+      interMax: 1500,
     };
   }
 
@@ -28,6 +30,20 @@ class App extends Component {
     )
   }
   
+  grade(stars) {
+        var i = 0;
+        var starIcon = [];
+        while (i < 5) {
+          if (stars > i) {
+            starIcon.push(<i key={i} class="material-icons gold starSize">star</i>);
+          } else {
+            starIcon.push(<i key={i} class="material-icons gold starSize">star_border</i>);
+          }
+          i++;
+        }
+      return  starIcon;
+  }
+  
   filterMenu() {
     return (
       <button class="filterMenu" onClick={() => this.setState({ filterMenuOn: !this.state.filterMenuOn })}>
@@ -37,33 +53,51 @@ class App extends Component {
     )
   }
   
-  filterList() {
+  filterList(low, high) {
     return (
       <div>
-        <button class="filterMenu" onClick={() => this.setState({ filterMenuState: 'Pris filter' })}> Inget filter </button>
-        <button class="filterMenu" onClick={() => this.setState({ filterMenuState: 'Pris 0 - 250 kr' })}> Pris 0 - 250 kr </button>
-        <button class="filterMenu" onClick={() => this.setState({ filterMenuState: 'Pris 250 - 500 kr' })}> Pris 250 - 500 kr </button>
-        <button class="filterMenu" onClick={() => this.setState({ filterMenuState: 'Pris 500 - 1000 kr' })}> Pris 500 - 1000 kr </button>
-        <button class="filterMenu" onClick={() => this.setState({ filterMenuState: 'Pris 1000 - Upp kr' })}> Pris 1000 - Upp kr </button>
+        <button class="filterMenu" onClick={() => this.setState({ interMin: 0, interMax: ' ' })}> Inget filter </button>
+        <button class="filterMenu" onClick={() => this.setState({ interMin: 1, interMax: 250 })}> Pris 0 - 250 kr </button>
+        <button class="filterMenu" onClick={() => this.setState({ interMin: 250, interMax: 500 })}> Pris 250 - 500 kr </button>
+        <button class="filterMenu" onClick={() => this.setState({ interMin: 500, interMax: 1000 })}> Pris 500 - 1000 kr </button>
+        <button class="filterMenu" onClick={() => this.setState({ interMin: 1000, interMax: ' ' })}> Pris 1000 - > kr </button>
       </div>
     )
+  }
+  
+  list() {
+    var lista = []
+      SalongList.map((salong) => {
+        if (salong.price > this.state.interMin && (salong.price < this.state.interMax || this.state.interMax === ' ')) {
+        lista.push( <div key={salong.id} class="filterMenu">
+         <div class="listHeight">{salong.time}</div>  
+         <div class="listBody">
+           {salong.title}
+           {this.low}
+           <br></br>
+           <div class="starSize">
+             {this.grade(salong.grade)}
+             {salong.grade}/5
+           </div>
+           {salong.adress}
+         </div> 
+         <div class="listHeight">{salong.price} kr</div>
+        </div>);
+        }
+      });
+    
+    return lista;
   }
     
   render() {
     return (
       <div className="App">
         {this.header()}
-        {this.line()}
+        <div class="gold"><hr></hr></div>
         {this.filterMenu()}
         {this.state.filterMenuOn && this.filterList()}
-        {this.line()}
-        <ul>
-         {
-           SalongList.map(function(movie){
-             return <li>{movie.id} - {movie.title}</li>;
-           })
-         }
-       </ul>
+        <div class="gold"><hr></hr></div>
+        {this.list()}
       </div>
     );
   }
